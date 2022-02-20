aaf1f264911b7d18723aef99d852731ae1e086c9
onthe6ks <onthe6ks@gmail.com>
2022-02-20 15:40:31 +0900

    learn props

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ColorfulMessage from "./componets/colorfulMessage";
+
+const App = () => {
+  const onClickButton = () => {
+    alert("alert");
+  };
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です!</ColorfulMessage>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;

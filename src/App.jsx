3ce4511ef742872238af8fc6dee30986af3c5b92
onthe6ks <onthe6ks@gmail.com>
2022-02-20 16:11:07 +0900

    learn re-render & useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./componets/colorfulMessage";
+// /* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./componets/colorfulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
@@ -14,6 +32,9 @@ const App = () => {
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
       <p>{num}</p>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
